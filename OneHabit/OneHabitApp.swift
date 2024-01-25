c8111ad8dcac819e0e9a57971a8db2e2ac0a838a
pjleonard37 <pjleonard37@gmail.com>
2024-01-24 20:38:06 -0500

    Initial Commit

---
+++ OneHabit/OneHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneHabitApp.swift
+//  OneHabit
+//
+//  Created by Patrick Leonard on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct OneHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
